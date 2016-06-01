671cc12be579dc8f80471a8fdc98274920d9386b
cahrsjo <cahrsjo@gmail.com>
2016-06-01 14:37:57 +0200

    initial commit

---
+++ src/config/strategies/local.strategy.js
@@ -0,0 +1,26 @@
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+    mongodb = require('mongodb').MongoClient;
+
+module.exports = function () {
+  passport.use(new LocalStrategy({
+      usernameField:'userName',
+      passwordField: 'password'
+  },
+  function (username, password, done) {
+      var url = 'mongodb://localhost:27017/libraryApp';
+
+      mongodb.connect(url, function(err, db) {
+          var collection = db.collection('users');
+          collection.findOne({username: username}, function (err, result) {
+              if(result.password === password) {
+                  var user = result;
+                  done(null, user);
+              } else {
+                  done(null, false, {massage:'Incorrect password'});
+              }
+          });
+      });
+  }
+  ));
+};
